ac2f17aec6d1ca4ec34dcb7e03c611e76701eb25
subratgupta <newsubrat@gmail.com>
2018-08-14 16:05:01 +0530

    V1.0 Beta on PlayStore

--- app/src/main/java/com/subratgupta/vsns/HomeActivity.java
+++ app/src/main/java/com/subratgupta/vsns/HomeActivity.java
@@ -6,10 +6,12 @@ import android.content.pm.PackageManager;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -19,6 +21,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -28,10 +32,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    public static DatabaseReference mDatabase;
+    public static FirebaseData appData;
     private static final Integer INTERNET = 1;
 
     @Override
@@ -41,14 +52,9 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        db_init(findViewById(R.id.youtube_layout));
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -58,6 +64,11 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        askForPermission(Manifest.permission.INTERNET, INTERNET);
+        WebView youtubeView = (WebView) findViewById(R.id.youtube_view);
+        youtubeView.getSettings().setJavaScriptEnabled(true);
+        youtubeView.loadUrl("https://m.youtube.com/channel/UCOAvF3WBpbGOBbuSn66o5cA");
     }
 
     @Override
@@ -86,6 +97,7 @@ public class HomeActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(),"Currently, this option is not available.",Toast.LENGTH_SHORT).show();
             return true;
         }
 
@@ -102,46 +114,64 @@ public class HomeActivity extends AppCompatActivity
         findViewById(R.id.donate_layout).setVisibility(View.GONE);
         findViewById(R.id.amount_section).setVisibility(View.GONE);
 
-        if (id == R.id.nav_home) {
+        /*if (id == R.id.nav_home) {
             // Handle the Home action
-        } else if (id == R.id.nav_youtube) {
-            findViewById(R.id.youtube_layout).setVisibility(View.VISIBLE);
-            askForPermission(Manifest.permission.INTERNET,INTERNET);
+        } else */
+        if (id == R.id.nav_youtube) {
+            askForPermission(Manifest.permission.INTERNET, INTERNET);
             WebView youtubeView = (WebView) findViewById(R.id.youtube_view);
             youtubeView.getSettings().setJavaScriptEnabled(true);
             youtubeView.loadUrl("https://m.youtube.com/channel/UCOAvF3WBpbGOBbuSn66o5cA");
+            db_init(findViewById(R.id.youtube_layout));
         } else if (id == R.id.nav_donate) {
-            if (MainActivity.appData.getEditable()){
-                findViewById(R.id.amount_section).setVisibility(View.VISIBLE);
-            }
-                findViewById(R.id.donate_layout).setVisibility(View.VISIBLE);
-            ((TextView) findViewById(R.id.header_donate)).setText(MainActivity.appData.getHeader());
-//            ((Button) findViewById(R.id.link_donate)).setText(MainActivity.appData.getLink());
-            ((TextView) findViewById(R.id.phone_donate)).setText(MainActivity.appData.getPhone());
-            ((Button) findViewById(R.id.link_donate)).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Uri uri;
-                    if (MainActivity.appData.getEditable()){
-                        RadioGroup amount_group = (RadioGroup) findViewById(R.id.amount_radio);
-                        int select_id = amount_group.getCheckedRadioButtonId();
-                        RadioButton radioButton = (RadioButton) findViewById(select_id);
-                        uri = Uri.parse("http://m.p-y.tm/requestPayment?recipient="+MainActivity.appData.getPhone()+"&amount="+radioButton.getText().toString()+"&comment="+MainActivity.appData.getComment_paytm());
-                    }
-                    else {
-                        uri = Uri.parse(MainActivity.appData.getLink());
-                    }
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
+            try {
+                if (appData.getEditable()) {
+                    findViewById(R.id.amount_section).setVisibility(View.VISIBLE);
                 }
-            });
-            Glide.with(getApplicationContext()).load(MainActivity.appData.getQr_link()).into((ImageView) findViewById(R.id.qr_code_donate));
-        } else if (id == R.id.nav_manage) {
+                findViewById(R.id.donate_layout).setVisibility(View.VISIBLE);
+                ((TextView) findViewById(R.id.header_donate)).setText(appData.getHeader());
+                ((TextView) findViewById(R.id.phone_donate)).setText(appData.getPhone());
+                ((Button) findViewById(R.id.link_donate)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        try {
+                            Uri uri;
+                            if (appData.getEditable()) {
+                                RadioGroup amount_group = (RadioGroup) findViewById(R.id.amount_radio);
+                                int select_id = amount_group.getCheckedRadioButtonId();
+                                RadioButton radioButton = (RadioButton) findViewById(select_id);
+                                uri = Uri.parse("http://m.p-y.tm/requestPayment?recipient=" + appData.getPhone() + "&amount=" + radioButton.getText().toString() + "&comment=" + appData.getComment_paytm());
+                            } else {
+                                uri = Uri.parse(appData.getLink());
+                            }
+                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                            startActivity(intent);
+                        } catch (Exception e) {
+                            db_init(findViewById(R.id.donate_layout));
+                        }
+
+                    }
+                });
+                Glide.with(getApplicationContext()).load(appData.getQr_link()).into((ImageView) findViewById(R.id.qr_code_donate));
+            } catch (Exception e){
+                Log.e("HomeActivity",e.getMessage()+"@donate");
+                Toast.makeText(getApplicationContext(),"Interne Disconnected!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Switch on Internet",Toast.LENGTH_SHORT).show();
+            }
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            try {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_SUBJECT, "VSNS");
+                String sAux = "VSNS (A little help can make a great change.)\n\n";
+                sAux = sAux + "https://play.google.com/store/apps/details?id=com.subratgupta.vsns";
+                i.putExtra(Intent.EXTRA_TEXT, sAux);
+                startActivity(Intent.createChooser(i, "choose one"));
+            } catch (Exception e) {
+                //e.toString();
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -164,7 +194,33 @@ public class HomeActivity extends AppCompatActivity
                 ActivityCompat.requestPermissions(HomeActivity.this, new String[]{permission}, requestCode);
             }
         } else {
-            Toast.makeText(this, "" + permission + " is already granted.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void db_init(final View view){
+        mDatabase.child("VSNS").child("appData").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    appData = dataSnapshot.getValue(FirebaseData.class);
+                    view.setVisibility(View.VISIBLE);
+                } catch (Exception e) {
+                    Log.e("HomeActivity", e.getMessage());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(),"No Internet Connection...",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        try{
+            appData.getEditable();
+        }
+        catch (Exception e){
+            view.setVisibility(View.GONE);
+            Toast.makeText(getApplicationContext(),"Oops! Internet Not Available",Toast.LENGTH_SHORT).show();
         }
     }
 }
